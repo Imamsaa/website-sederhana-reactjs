e98b21e21278922b92a7fcae9c80a3219e1c7cb9
Imamsaa <imamsafii2222022@gmail.com>
2026-09-14 22:56:29 +0700

    feat:menambahkan section home dengan html-react-parser

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,21 @@
 import "./../styles/Home.css"
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { homeSection } from "../data/HomeSection";
+import parse from 'html-react-parser'
 
 function Home() {
     return (
         <>
             <Navbar/>
+            <div className="warpper">
+                <section id="home">
+                    <img src={homeSection.image}/>
+                    <div className="kolom">
+                        {parse(homeSection.content)}
+                    </div>
+                </section>
+            </div>
             <Footer/>
         </>
     );
