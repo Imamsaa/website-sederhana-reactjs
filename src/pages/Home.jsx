c28eb9e98830aaf642c066c6ac4e52bfabc8e0af
Imamsaa <imamsafii2222022@gmail.com>
2026-09-14 23:46:05 +0700

    feat:membuat courses section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,28 @@ import "./../styles/Home.css"
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { homeSection } from "../data/HomeSection";
+import { coursesSection } from "../data/CoursesSection";
 import parse from 'html-react-parser'
 
 function Home() {
     return (
         <>
             <Navbar/>
-            <div className="warpper">
+            <div className="wrapper">
+                {/* home */}
                 <section id="home">
                     <img src={homeSection.image}/>
                     <div className="kolom">
                         {parse(homeSection.content)}
                     </div>
+                </section>
+
+                {/* online course */}
+                <section id="courses">
+                    <div className="kolom">
+                        {parse(coursesSection.content)}
+                    </div>
+                    <img src={coursesSection.image}/>
                 </section>
             </div>
             <Footer/>
